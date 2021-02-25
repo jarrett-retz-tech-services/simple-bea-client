006775a7e4c155ffde5bc7f7eddd42e735ea6870
jdretz <jarrett.retz@comcast.net>
2021-02-24 19:56:49 -0800

    init commit

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+
+long_description = (here / 'README.md').read_text(encoding='utf-8')
+
+setup(
+    name='beasy',  # Required
+    version='0.0.1a4',  # Required
+    description='Simple Python API client for accessing data on the Bureau of Economic Analysis application programming interface.',  # Optional
+    long_description=long_description,  # Optional
+    long_description_content_type='text/markdown',  # Optional (see note above)
+    url='https://github.com/jdretz/simple-bea-client',  # Optional
+    author='Jarrett Retz',  # Optional
+    author_email='jretz@jrts.info',  # Optional
+    classifiers=[  # Optional
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+    packages=find_packages(where='src'),  # Required
+    python_requires='>=3.6, <4',
+)
